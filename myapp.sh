387d722ac6e27d5897dd2c6f2c0a457ca400331b
ketanpatole77 <ketanpatole77@gmail.com>
2024-11-30 22:32:58 +0530

    Update myapp.sh

--- myapp.sh
+++ myapp.sh
@@ -2,3 +2,12 @@
 # This is bash program to display Hello World
 echo " Hello World from ketan "
 #this line is to verify auto deploy on ec2
+#git starting a repository 
+git init 
+git status 
+#git branch 
+git branch 
+git branch <branch name>
+git checkout <branch-name>
+git merge <branch-name>
+git checkout -b <branch-name>
